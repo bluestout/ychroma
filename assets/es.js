f56e8e520dfa183ee7e9404098d84ae411727ee5
ecomdevstar <kieshalortz@gmail.com>
2025-01-02 10:29:00 -0800

    Cart - Risk of Purchase

--- assets/es.js
+++ assets/es.js
@@ -75,3 +75,27 @@ function changeImageColor(e){
     })  
   }
 }
+
+upcartOnCartLoaded = (cart) => {
+	const widget = document.querySelector('#CartPopup');
+	const container = widget.querySelector('.upcart-checkout-button-container');
+	const message = `
+		<div class="upcart-es-message-wrapper">
+			<div class="upcart-es-message-wrapper--item">
+				<img src="https://cdn.shopify.com/s/files/1/0620/1648/7614/files/Group_42.svg?v=1734504740">
+				<span>100% Satisfaction Guarantee</span>
+			</div>
+			<div class="upcart-es-message-wrapper--item">
+				<img src="https://cdn.shopify.com/s/files/1/0620/1648/7614/files/Group_41.svg?v=1734504740">
+				<span>Hassle-Free <br>90-Day Returns</span>
+			</div>
+		</div>
+	`;
+
+	console.log(container);
+
+	const messageElement = document.createElement('div');
+	messageElement.innerHTML = message;
+
+	container.append(messageElement);
+}
